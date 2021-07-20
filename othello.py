bc455e293de72fe81691241c209317c651569951
Logan Patterson <67162730+LoganTylerPatterson@users.noreply.github.com>
2021-07-20 14:19:53 -0700

    Update othello.py
    Almost Finished!

--- othello.py
+++ othello.py
@@ -29,28 +29,40 @@ class Othello:
         self.board[4][4] = "player"
 
     """Main game loop to wrange all this sheet"""
+
     def run_game(self):
         self.draw_initial_board()
         self.turn = "player"
 
         """Start the main loop for the game."""
         while True:
-            #Watch for mouse events
+            # Watch for mouse events
             if self.turn == "player":
-               if self.check_for_events():
+                self.show_hints()
+                if self.check_for_events():
                    self.get_player_move()
+                   self.update_board()
                    self.check_win()
                    self.turn = "computer"
             elif self.turn == "computer":
-                self.get_computer_move()
+                x, y = self.get_computer_move()
+                pygame.time.delay(1000)
+                self.make_move(self.board, "computer", x, y)
+                self.update_board()
                 self.check_win()
                 self.turn = "player"
 
             pygame.display.flip()
 
+    def show_hints(self):
+        p_moves = self.get_valid_moves(self.board, "player")
+        print(p_moves)
+        for tile in p_moves:
+            p_y, p_x = tile
+            self.draw_square(p_y, p_x, self.c_black)
+
     """Given a board and a player, it will iterate through the board and return the score of said board"""
     def get_player_score(self, board, tile):
-        print(">>Get player score")
         score = 0
         for row in range(self.height):
             for col in range(self.width):
@@ -60,18 +72,17 @@ class Othello:
 
     """This method will iterate through the valid moves, determine which gives the highest value, and returns a tuple of the x and y coordinates"""
     def get_computer_move(self):
-        print(">>Get Computer Move")
         bestx = -1
         besty = -1
         valid_moves = self.get_valid_moves(self.board, "computer")
         random.shuffle(valid_moves)
 
-        #Going for the corner is the best option
+        # Going for the corner is the best option
         for x, y in valid_moves:
             if self.is_on_corner(x, y):
                 return [x, y]
 
-        #Get the highest scoring move
+        # Get the highest scoring move
         best_score = -1
 
         for x, y in valid_moves:
@@ -87,9 +98,7 @@ class Othello:
         return [bestx, besty]
 
     """Gives us a copy of the board so the computer can do some nice testing"""
-    def get_board_copy(self, board):
-        print(">>Get Board Copy")
-
+    def get_board_copy(self, board): 
         board_copy = [[" "] * 8,[" "] * 8,[" "] * 8,[" "] * 8,[" "] * 8,[" "] * 8,[" "] * 8,[" "] * 8]
 
         for x in range(self.width):
@@ -99,8 +108,6 @@ class Othello:
 
     """Takes a board, player, x and y and updates that tile along with all the tiles that should be flipped """
     def make_move(self, board, tile, x, y):
-        print(">>Make Move")
-
         tiles_to_change = self.is_valid_move(board, tile, x, y)
 
         if tiles_to_change == False:
@@ -113,18 +120,14 @@ class Othello:
 
     """Pretty simple"""
     def is_on_corner(self, x, y):
-        print(">>Is On Corner")
-
         return (x == 0 or x == self.width - 1 and y == 0 or y == self.height - 1)
 
     """If neither the player nor the computer can move, game is over; returns True if play continues"""
     def check_win(self):
-        print(">>Check Win")
-
         player_can_move = self.get_valid_moves(self.board, "player")
         computer_can_move = self.get_valid_moves(self.board, "computer")
         if player_can_move == [] or computer_can_move == []:
-            return True
+            self.run_game()
 
     """Looks for any player events, returns true if so"""
     def check_for_events(self):
@@ -150,8 +153,6 @@ class Othello:
 
     """Not too hard to decipher"""
     def get_random_starter(self):
-        print(">>Get Random Starter")
-
         turn = random.randint(0, 1)
         if turn == 1:
             return "player"
@@ -160,13 +161,12 @@ class Othello:
 
     """Iterates through the board, checks each to see if it is a valid move, returns a list of valid moves"""
     def get_valid_moves(self, board, tile):
-        print(">>Get Valid Moves")
-
         valid_moves = []
         for row in range(self.width):
             for col in range(self.height):
                 if self.is_valid_move(board, tile, row, col):
                     valid_moves.append([row, col])
+        
         return valid_moves
 
     """returns True/False depending on whether the row and column are inside/outside the board"""
@@ -175,7 +175,7 @@ class Othello:
 
     """This method returns False if the move is not valid and a list of the tiles that need to be flipped if it is valid"""
     def is_valid_move(self, board, tile, row_start, col_start):
-        #This is working
+        # This is working
         if board[row_start][col_start] != " " or not self.is_on_board(row_start, col_start):
             return False
 
@@ -221,7 +221,6 @@ class Othello:
 
     """Draws the grid lines with 4 initial squares filled in in the center of the grid"""
     def draw_initial_board(self):
-        print(">>Draw Initial Board")
         self.screen.fill(self.c_white)
         line_width = 150
         # Draw game grid
@@ -241,25 +240,24 @@ class Othello:
 
     """Draws a square at the given position on the board"""
     def draw_square(self, row, col, color):
-        print(">>Draw Square")
         y = (row) * 150 + 3
         x = (col) * 150 + 3
         pygame.draw.rect(self.screen, color, (x, y, 146, 146))
 
     """After a element has been updated in self.board, this method will redraw the entire grid -> flip() will need to be called"""
     def update_board(self):
-        print(">>Update Board")
-        for col in range(self.height):
-            for row in range(self.width):
-                square = self.board[col][row]
-                if square == "player":
-                    self.draw_square(col, row, self.c_blue)
-                elif square == "computer":
-                    self.draw_square(col, row, self.c_red)
+        for row in range(self.height):
+            for col in range(self.width):
+                tile = self.board[row][col]
+                if tile == "player":
+                    self.draw_square(row, col, self.c_blue)
+                elif tile == "computer":
+                    self.draw_square(row, col, self.c_red)
+                else:
+                    self.draw_square(row, col, self.c_white)
 
     """Iterates through the board and updates the players score by one each time their name is found"""
     def get_score(self):
-        print(">>Get Score")
         for row in range(self.height):
             for col in range(self.width):
                 square = self.board[row][col]
